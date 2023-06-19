0e7ba60872380b1e19579c1917f490aef41ee688
Guilherme <guilhermefirme3@gmail.com>
2023-06-18 23:52:39 -0300

    feat: adicionando funcionalidades para se jogar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
+import React, { useState } from "react";
+import styled from "styled-components";
 
 const Container = styled.div`
   display: flex;
@@ -15,7 +15,11 @@ const Board = styled.div`
   gap: 10px;
 `;
 
-const Cell = styled.div`
+type CellProps = {
+  onClick: (row: number, column: number) => void;
+};
+
+const Cell = styled.div<CellProps>`
   display: flex;
   align-items: center;
   justify-content: center;
@@ -32,42 +36,121 @@ const Message = styled.div`
 `;
 
 const initialBoard = [
-  ['', '', ''],
-  ['', '', ''],
-  ['', '', ''],
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""],
 ];
+
+const checarGanhador = (board: string[][]) => {
+  const todasCombinacoes = [
+    // Linha
+    [[0, 0], [0, 1], [0, 2]],
+    [[1, 0], [1, 1], [1, 2]],
+    [[2, 0], [2, 1], [2, 2]],
+    // Coluna
+    [[0, 0], [1, 0], [2, 0]],
+    [[0, 1], [1, 1], [2, 1]],
+    [[0, 2], [1, 2], [2, 2]],
+    // Diagonal
+    [[0, 0], [1, 1], [2, 2]],
+    [[0, 2], [1, 1], [2, 0]]
+  ];
+  
+  let msgGanhador = "";
+  
+  // Check for a winner
+  for (const combination of todasCombinacoes) {
+    const [x1, y1] = combination[0];
+    const [x2, y2] = combination[1];
+    const [x3, y3] = combination[2];
+  
+    if (
+      board[x1][y1] === board[x2][y2] &&
+      board[x2][y2] === board[x3][y3] &&
+      board[x1][y1] !== ""
+    ) {
+      msgGanhador = "O Ganhador é " + board[x1][y1];
+      break;
+    }
+  }
+  
+  // Check for a draw
+  if (msgGanhador === "") {
+    let boardCheio = true;
+    for (const row of board) {
+      for (const cell of row) {
+        if (cell === "") {
+          boardCheio = false;
+          break;
+        }
+      }
+      if (!boardCheio) {
+        return msgGanhador;
+      }
+    }
+    if (boardCheio) {
+      msgGanhador = "O jogo terminou em empate";
+    }
+  }
+  return msgGanhador;
+};
 
 const App = () => {
   const [board, setBoard] = useState(initialBoard);
-  const [player, setPlayer] = useState('X');
+  const [player, setPlayer] = useState("X");
+  const [ganhador, setGanhador] = useState("");
 
   const restartGame = () => {
-    // to-do
+    setBoard([
+      ["", "", ""],
+      ["", "", ""],
+      ["", "", ""],
+    ]);
+    setPlayer("X");
+    setGanhador("");
+  };
+
+  const buttonHandler = (row: number, column: number) => {
+    if (board[row][column] != "") {
+      return;
+    }
+    board[row][column] = player;
+    setBoard(board);
+
+    let possivelGanhador = checarGanhador(board);
+
+    if (possivelGanhador != "") {
+      setGanhador(possivelGanhador);
+    } else {
+      setPlayer((prev) => (prev === "X" ? "O" : "X"));
+    }
   };
 
   return (
     <Container>
+      <Message>É a vez do jogador {player}</Message>
+      <br></br>
       <Board>
-        <Cell></Cell>
-        <Cell></Cell>
-        <Cell></Cell>
-        <Cell></Cell>
-        <Cell></Cell>
-        <Cell></Cell>
-        <Cell></Cell>
-        <Cell></Cell>
-        <Cell></Cell>
+        <Cell onClick={() => buttonHandler(0, 0)}> {board[0][0]} </Cell>
+        <Cell onClick={() => buttonHandler(0, 1)}> {board[0][1]} </Cell>
+        <Cell onClick={() => buttonHandler(0, 2)}> {board[0][2]} </Cell>
+        <Cell onClick={() => buttonHandler(1, 0)}> {board[1][0]} </Cell>
+        <Cell onClick={() => buttonHandler(1, 1)}> {board[1][1]} </Cell>
+        <Cell onClick={() => buttonHandler(1, 2)}> {board[1][2]} </Cell>
+        <Cell onClick={() => buttonHandler(2, 0)}> {board[2][0]} </Cell>
+        <Cell onClick={() => buttonHandler(2, 1)}> {board[2][1]} </Cell>
+        <Cell onClick={() => buttonHandler(2, 2)}> {board[2][2]} </Cell>
       </Board>
+
+      <Message>{ganhador}</Message>
+
       <Message>
-        O jogador "" venceu!
-        <br />
         <button onClick={restartGame}>Reiniciar</button>
-      </Message>
-      <Message>
-        É a vez do jogador "{player}"
       </Message>
     </Container>
   );
 };
 
 export default App;
+
+//<Message>O Jogador {player} venceu! <button onClick={restartGame}>Reiniciar</button></Message>
